e26e2bec62d2cfedd662eeb65e11ca176e75b3d0
Divyansh Harsh <90635122+divyanshharsh@users.noreply.github.com>
2024-02-01 21:08:24 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "2751b27324a41fe5d438d173b2ca3349",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
